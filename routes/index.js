fd4bac10984c684698a4e2efda636ca02de2f55e
MatthewScott <mattwillscott95@gmail.com>
2022-07-20 19:23:46 -0400

    sending up the homework pre john

---
+++ routes/index.js
@@ -0,0 +1,18 @@
+const { Router } = require('express')
+const controllers = require('../controllers')
+const router = Router()
+
+router.get('/', (req, res) => res.send('This is root!'))
+
+router.post('/plants', controllers.createPlant)
+
+router.get('/plants', controllers.getAllPlants)
+
+router.get('/plants/:id', controllers.getPlantById)
+
+router.put('/plants/:id', controllers.updatePlant)
+router.delete('/plants/:id', controllers.deletePlant)
+
+module.exports = router
+
+//test plant id: 62d87c67cc6e61817c0bce9a
